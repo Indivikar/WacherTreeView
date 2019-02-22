555d9ae69d46d2ff26ee7f13ceb91b4c6c14bd58
Dominik <d.hirt@indivikar.ch>
2019-02-22 11:35:27 +0100

    v0.0.27 - TODO-Liste geändert

--- src/app/StartWacherDemo.java
+++ src/app/StartWacherDemo.java
@@ -13,32 +13,6 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-// Getestet
-// - Multi-Select DragNDrop  Desktop > Programm
-
-// TODO - Probleme beim verschieben -> wenn ich ein Ordner per DragNDrop intern verschiebe zum beispiel "A0" dann wird er in ein Ordner unter "A0" verschoben
-// TODO - DragNDrop -> Programm > Programm, MoveOrCopy ausprogrammieren
-// TODO - beim starten der app mit Threads arbeiten, um den Inhalt zu laden und cursor wait einbauen
-// TODO - Methode entwickeln wo alle logfile-pfade, die vom client angelegt wurden, in eine Liste gespeichert werden, 
-//			- beim beenden des programmes, alle logfiles löschen  
-//			- im ContextMenu ein item einrichten "unlock" alle logfiles löschen die vom client angelegt wurden
-//			- für alle clienten -> bei refresh alle logfles löschen
-// TODO - alle Stages oder dialoge auf showAndWait setzen
-// TODO - LockFile -> wenn ein programm geändert wird, dann soll das logfile gelöscht werden
-// TODO - ab und zu kommt ein fehler, wenn bei einem Client die ganze zeit das rename-fenster auf ist und ein anderer client beendet und neu gestartet wird,
-//			ist wohl in der zeile 183 klasse CRename "this.pathTreeCell.getTreeItem().getValue().getLevelOneItem());"
-
-
-// gefixte Fehler
-// OK TODO - es gibt noch Probleme beim popupmenu, manche items werden nicht aktiv, obwohl sie es sein sollten
-// OK TODO - bei Rename -> wenn der name ein leerzeichen am ende hat, gibt es ein Fehler z.B.: "new "
-// OK TODO - DragNDrop -> Programm > Programm, wenn es bei MoveOrCopy die Files schon gibt, blockt der Dialog das Fenster nicht
-// OK TODO - Multi-Select bei Rename meldung einbauen, das nur ein item selectet werden darf
-// OK TODO - Multi-Select DragNDrop vom Programm auf den Desktop klappt noch nicht
-// OK TODO - bei copy oder move den Ordner locken
-// OK TODO - bei Rename nach lockfiles schauen
-// OK TODO - DragNDrop unterbinden, wenn files gelockt sind
-// OK TODO - multi-delete -> wird nur eine datei gelockt und bei cancel wird das lock-icon nicht gewechselt, auf unlock
 
 public class StartWacherDemo  extends Application {
 
